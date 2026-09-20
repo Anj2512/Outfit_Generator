8face9600a78e7e52bc906aa1d2b7e1e706263c7
Anj2512 <joshianvi9@gmail.com>
2026-09-20 13:02:05 -0400

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,28 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        plugins {
+            id("com.android.application") version "8.4.0" // or whatever latest version you're using
+            id("com.google.gms.google-services") version "4.4.2"
+        }
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "FinalProject"
+include(":app")
+ 
